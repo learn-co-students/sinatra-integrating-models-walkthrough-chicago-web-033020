69473221782e6fc2259f70580112eea9c7498490
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-22 15:01:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+# Cpmtroller
+# get '/' do, which renders the index.erb page.
+# post '/' do, which receives the form data through params and renders the results page.
 
 class App < Sinatra::Base
   get '/' do
@@ -7,8 +10,8 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
     erb :results
   end
+
 end
